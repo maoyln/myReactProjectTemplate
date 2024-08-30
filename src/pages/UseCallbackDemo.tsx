620d1cd3dabfda8b898d2a26f5ce88674dbc62fe
maoyln <maoyl_yx@163.com>
2024-08-30 16:28:32 +0800

    useCallBack的用法demo

--- src/pages/UseCallbackDemo.tsx
+++ src/pages/UseCallbackDemo.tsx
@@ -4,6 +4,7 @@ const UseCallbackDemo: React.FC = () => {
   const [count, setCount] = useState(0);
 
   // callback函数只在组件首次渲染时创建一次，即使App组件重新渲染，Counter组件也不会因为callback的改变而重新渲染。
+  // 但是有一个弊端，Counter不更新，其中的props的数据也不会重新渲染
   const callback = useCallback(() => {
     console.log('Callback called');
     setCount(count + 1)
