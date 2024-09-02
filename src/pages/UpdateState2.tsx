57fd9e21bf70736f654608664085cd0b8dd75f4b
maoyln <maoyl_yx@163.com>
2024-09-02 15:48:21 +0800

    state异步更新

--- src/pages/UpdateState2.tsx
+++ src/pages/UpdateState2.tsx
@@ -31,6 +31,13 @@ const UpdateState2: React.FC = () => {
       )
     );
   }
+
+  const incrementTwice = () => {
+    setText(text + 1);
+    setText(text + 2);
+    // 由于 setState 是异步的，this.state.text 在第二次 setState 调用时可能并未更新。因此，在没有合并的情况下，this.state.text 仍然是原始值
+    // 最终两次结果合并，最终以setText为准
+  }
   
   console.log(spanRef.current);
   console.log('render');
@@ -49,6 +56,7 @@ const UpdateState2: React.FC = () => {
       </button>
       <button onClick={() =>setText(null)}>useState null</button> 
       <button onClick={() => setText(text + 1)}>a+1</button>
+      <button onClick={incrementTwice}>两次加1</button>
       {/* <button onClick={() => createPortalRender()}>createPortalRender</button> */}
 
       <div id="father">
