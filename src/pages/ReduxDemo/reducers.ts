49e5268a9d32820ebfccfaad4661bf28c73cc3dc
maoyln <maoyl_yx@163.com>
2024-09-04 11:31:48 +0800

    优化代码

--- src/pages/ReduxDemo/reducers.ts
+++ src/pages/ReduxDemo/reducers.ts
@@ -1,4 +1,4 @@
-import { ADD_TODO, AddTodoAction, addTodo } from './actions';
+import { ADD_TODO, AddTodoAction } from './actions';
 import { AppState, Todo,  } from './types';
 import { combineReducers } from 'redux';
 
