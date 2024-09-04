ffa507e6cf8ded61911f53c0a28c02258abf9dad
maoyln <maoyl_yx@163.com>
2024-09-04 11:13:22 +0800

    创建 Reducer

---
+++ src/pages/ReduxDemo/reducers.ts
@@ -0,0 +1,20 @@
+import { ADD_TODO, AddTodoAction } from './actions';
+import { AppState, Todo, addTodo } from './types';
+import { combineReducers } from 'redux';
+
+const todosReducer = (state: AppState['todos'] = [], action: AddTodoAction): Todo[] => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, {id: state.length, text: action.payload.text}];
+    default:
+      return state;
+  }
+}
+
+// 使用 combineReducers 将 todos 减化器组合成根减化器
+const rootReducer = combineReducers({
+  todos: todosReducer,
+});
+
+// 导出根减化器
+export default rootReducer;
