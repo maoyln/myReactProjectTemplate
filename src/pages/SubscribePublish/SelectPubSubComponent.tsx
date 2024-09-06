85232214f136a1f52eba49c6ffa7d36208e9613e
maoyln <maoyl_yx@163.com>
2024-09-06 18:57:25 +0800

    发布订阅模式详解

--- src/pages/SubscribePublish/SelectPubSubComponent.tsx
+++ src/pages/SubscribePublish/SelectPubSubComponent.tsx
@@ -11,29 +11,36 @@ interface SelectPubSubProps {
   selectOptions: SelectOption[];
 }
 
+// 创建 PubSub 的单例
 const pubSubInstance = new PubSub();
 
 const SelectPubSubComponent: React.FC<SelectPubSubProps> = ({ selectOptions }) => {
+  // 状态用于存储当前被选中的值
   const [selectedValue, setSelectedValue] = useState<string>('');
 
+  // 当选项改变时，设置默认选中值
   useEffect(() => {
     if (selectOptions.length) {
       setSelectedValue(selectOptions[0].value);
     }
   }, [selectOptions]);
 
+  // 设置监听器，当有 'change' 事件时更新状态
   useEffect(() => {
     const changeHandler = (newValue: string) => setSelectedValue(newValue);
     pubSubInstance.subscribe('change', changeHandler);
 
     return () => {
+      // // 清理操作：取消订阅
       pubSubInstance.unsubscribe('change', changeHandler);
     };
   }, []);
 
+  // 选择框变化时的处理函数
   const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const newValue = event.target.value;
     setSelectedValue(newValue);
+    // 发布 'change' 事件，传递新的值
     pubSubInstance.publish('change', newValue);
   };
 
