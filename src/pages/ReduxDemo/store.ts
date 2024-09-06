5b83e572f58fb427fee6dc169cd0bb30e1a48a12
maoyln <maoyl_yx@163.com>
2024-09-06 15:19:32 +0800

    优化代码

--- src/pages/ReduxDemo/store.ts
+++ src/pages/ReduxDemo/store.ts
@@ -2,6 +2,7 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers'; // 引入根减化器
 
-// 创建并导出 store，这是应用的单一数据源
+// 创建并导出 store，这是应用的单一数据源 
+// TODO createStore方法过期
 const store = createStore(rootReducer);
 export default store;
