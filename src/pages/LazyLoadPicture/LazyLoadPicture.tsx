0df1c3a44a6e244f1dd64d815b250920c83d4f0d
maoyln <maoyl_yx@163.com>
2024-10-29 15:59:32 +0800

    图片的懒加载优化

--- src/pages/LazyLoadPicture/LazyLoadPicture.tsx
+++ src/pages/LazyLoadPicture/LazyLoadPicture.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from 'react';
-import Loading from './loading.png';
+import React, { useEffect, useRef } from "react";
+import Loading from "./loading.png";
 
 const LazyLoadImages: React.FC = () => {
   const imgRefs = useRef<HTMLImageElement[]>([]);
@@ -8,35 +8,40 @@ const LazyLoadImages: React.FC = () => {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
     const winHeight = window.innerHeight;
 
+    // 判断当前当前图片是否在可视区域
     imgRefs.current.forEach((img) => {
       if (img && img.offsetTop < scrollTop + winHeight) {
         setTimeout(() => {
           // 注意⚠️： 该定时器是模拟网络请求的消耗时间，实际操作的时候一定要去掉
-          img.src = img.getAttribute('data-src') || '';
+          img.src = img.getAttribute("data-src") || "";
         }, 100);
       }
     });
   };
 
+  /**
+   * 初始化页面的时候在当前页面中的图片正常加载展示
+   */
   const initializeLoad = () => {
     const winHeight = window.innerHeight;
     imgRefs.current.forEach((img) => {
+      // 判断当前当前图片是否在可视区域
       if (img && img.offsetTop < winHeight) {
-        img.src = img.getAttribute('data-src') || '';
+        img.src = img.getAttribute("data-src") || "";
       }
     });
   };
 
   useEffect(() => {
     // 添加滚动事件监听
-    window.addEventListener('scroll', lazyLoad);
+    window.addEventListener("scroll", lazyLoad);
 
     // 初始加载在屏幕内的图像
     initializeLoad();
 
     // 组件卸载时移除事件监听
     return () => {
-      window.removeEventListener('scroll', lazyLoad);
+      window.removeEventListener("scroll", lazyLoad);
     };
   }, []);
 
