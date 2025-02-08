4581ef2c213857b3d8c1702beefedc487a4f4352
maoyln <maoyl_yx@163.com>
2025-02-08 15:11:31 +0800

    useCallBack和mmemo

--- src/pages/useMemoDemo/UseMemoDemo2.tsx
+++ src/pages/useMemoDemo/UseMemoDemo2.tsx
@@ -10,6 +10,7 @@ const slowFunction = (num: number) => {
 const UseMemoDemo2: React.FC<IProps> = (props: IProps) => {
   const [count, setCount] = useState(0);
   const [dark, setDark] = useState(false);
+  const [text, setText] = useState("");
 
   // 使用 useMemo 缓存计算结果，只有 count 变化时才会重新计算
   const doubleCount = useMemo(() => slowFunction(count), [count]);
@@ -17,8 +18,10 @@ const UseMemoDemo2: React.FC<IProps> = (props: IProps) => {
     <div>
       <p>Double Count: {doubleCount}</p>
       <p>黑色: {dark ? '是' : '否'}</p>
+      <p>text: {text}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setDark((prev) => !prev)}>Toggle Theme</button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
     </div>
   );
 };
